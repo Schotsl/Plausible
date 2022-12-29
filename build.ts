ac6b0b06ce8c08a7aec50fb1634120c7289d0aca
Sjors van Holst <sjorsvanholst@gmail.com>
2022-12-29 15:41:13 +0100

    🐛 Fixed release

---
+++ build.ts
@@ -0,0 +1,22 @@
+import { build } from "https://deno.land/x/dnt@0.22.0/mod.ts";
+
+await build({
+  shims: {
+    deno: true,
+  },
+  package: {
+    name: "plausible",
+    author: "Sjors van Holst",
+    license: "MIT",
+    version: "1.0.0",
+    homepage: "https://github.com/Schotsl/Plausible",
+    repository: "git://github.com/Schotsl/Plausible.git",
+    description:
+      "A simple Deno and Node wrapper for the recently released Plausible API",
+  },
+  outDir: "./dist",
+  entryPoints: ["./index.ts"],
+});
+
+Deno.copyFileSync("README.md", "dist/README.md");
+Deno.copyFileSync("LICENSE.md", "dist/LICENSE.md");
