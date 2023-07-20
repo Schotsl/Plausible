b044753ebe82053c882a1ebd7da1da2ed7ad584b
GitHub <noreply@github.com>
2023-07-20 00:10:39 +0000

    ⬆️ Update dependencies

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import Plausible from "./index.ts";
 
-import { assert } from "https://deno.land/std@0.194.0/testing/asserts.ts";
+import { assert } from "https://deno.land/std@0.195.0/testing/asserts.ts";
 import { initializeEnv } from "./helper.ts";
 
 // Initialize environment variables for test runs
